35d37102e74f1ffa56ec9ceb32f186e263491e43
Michal Zagrodzki <michalzagrodzki@gmail.com>
2020-04-29 21:13:54 +0200

    Refactor handling of getting user details

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,13 @@ var graph = require('./../utils/graph');
 /* GET user details. */
 const user = {
   get: async function(req, res, next) {
-    const accessToken = req.headers.authorization
-    const user = await graph.getUserDetails(accessToken);
-    await res.status(200).send(user);
+    try {
+      const accessToken = req.headers.authorization
+      const user = await graph.getUserDetails(accessToken);
+      await res.status(200).send(user);
+    } catch (error) {
+      res.status(401).send(error);
+    }
   }
 }
 
